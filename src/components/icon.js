8ce4da3d6c85a9b40a13ba5a468b86b771b69e56
Riley Weber <weber@midtrade.net>
2018-06-09 12:55:45 -0400

    updates

---
+++ src/components/icon.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Icon = ({ prefix, name }) => <i className={`${prefix} ${name}`} />;
+
+export default Icon;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Icon from './components/icon';
 import './index.css';
 
 function Page() {
@@ -8,20 +9,40 @@ function Page() {
             <Name/>
             <div/>
             <Info/>
+
+            <Links/>
         </div>
     );
 }
 
-function Info () {
+function Info() {
     return (
         <div className='section info'>
-            Hey, my name is Riley.
-            <br/><br/>
-            I've been a writing code since high school, but more formally since my freshman year at the Naval Academy.
+            I've been writing code since high school.<br/><br/>
+            I started a tech company with a friend while we were at the Naval Academy.<br/>
+            I dropped out of college to be a software engineer in Florida.<br/>
+            I'm now a web application developer in Philadelpha.
         </div>
     );
 }
 
+function Links() {
+    const linkedin = <LinkIcon prefix='fab' name='fa-linkedin' link='https://www.linkedin.com/in/riley-weber-b65072b7'/>;
+    const email    = <LinkIcon prefix='far' name='fa-envelope' link='mailto:rileypweber@gmail.com'/>;
+
+    return (
+        <div className='connect-links'>
+            {linkedin} {email}
+        </div>
+    );
+}
+
+
+const LinkIcon = ({ prefix, name, link }) => (
+    <a href={link} className='rw-icon' target='_blank'>
+        <Icon prefix={prefix} name={name}/>
+    </a>
+);
 const Name = () => <div className='section name'>Riley Weber</div>;
 
 ReactDOM.render(<Page/>, document.querySelector('#root'));
